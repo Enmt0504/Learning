6440381a46575dc8fe36c47b84b0bf39743798e6
Tartrazine1024 <tartrazine.yellow@gmail.com>
2017-06-21 14:11:25 +0900

    新規プロジェクトを追加

---
+++ FavoriteApps/app/src/main/java/com/learning/enmt0504/favoriteapps/MainActivity.java
@@ -0,0 +1,16 @@
+package com.learning.enmt0504.favoriteapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by enomoto on 17/06/21.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ FavoriteApps/app/src/main/java/com/learning/enmt0504/favoriteapps/MainFragment.java
@@ -0,0 +1,10 @@
+package com.learning.enmt0504.favoriteapps;
+
+import android.support.v17.leanback.app.BrowseFragment;
+
+/**
+ * Created by enomoto on 17/06/21.
+ */
+
+public class MainFragment extends BrowseFragment {
+}
